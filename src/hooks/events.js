0a1d8e30358d9ca9a618154a5bd2aa48dca6bdc2
Cweili <3weili@gmail.com>
2020-10-26 14:45:06 +0800

    feat: before pre after event handler

--- src/hooks/events.js
+++ src/hooks/events.js
@@ -1,40 +1,52 @@
 import { useEffect } from 'react'
 
-import { upperFirst } from '../utils'
-
-const handlerNameToEventName = (string) =>
-  string.substr(2, 1).toLowerCase() + string.substr(3)
-
+const eventHandlerPrefixRE = /^on(\w+)$/
+const getEventName = (propName) => {
+  const name = eventHandlerPrefixRE.exec(propName)
+  return name && name[1].toLowerCase()
+}
+const forEachHandler = (minder, props, cb) => {
+  if (minder) {
+    Object.keys(props).forEach((propName) => {
+      const eventName = getEventName(propName)
+      if (eventName && handlerPropKeys.indexOf(eventName) >= 0) {
+        cb(eventName, propName)
+      }
+    })
+  }
+}
 const eventNames = (
-  'click,dblclick,mousedown,mousemove,mouseup,keydown,keyup,keypress,touchstart,touchend,touchmove,' +
-  'beforeExecCommand,preExecCommand,afterExecCommand,' +
+  'click,dblclick,mousedown,mousemove,mouseup,mousewheel,keydown,keyup,keypress,touchstart,touchend,touchmove,' +
+  'execcommand,' +
   'selectionchange,contentchange,interactchange,' +
   'editnoterequest,shownoterequest,hidenoterequest,' +
   'nodechange,editnode'
 ).split(',')
-const handlerPropKeys = eventNames.map(
-  (eventName) => `on${upperFirst(eventName)}`
+let handlerPropKeys = []
+eventNames.map(
+  (eventName) => {
+    handlerPropKeys = handlerPropKeys.concat([
+      eventName,
+      `before${eventName}`,
+      `pre${eventName}`,
+      `after${eventName}`
+    ])
+  }
 )
 
 // 数据
 export default function useEvents(minder, props) {
   // 事件监听
   useEffect(() => {
-    if (minder) {
-      handlerPropKeys.forEach((handlerProp) => {
-        if (props[handlerProp]) {
-          minder.on(handlerNameToEventName(handlerProp), props[handlerProp])
-        }
-      })
-    }
+    const handlers = {}
+    forEachHandler(minder, props, (eventName, propName) => {
+      handlers[eventName] = props[propName]
+      minder.on(eventName, props[propName])
+    })
     return () => {
-      if (minder) {
-        handlerPropKeys.forEach((handlerProp) => {
-          if (props.handlerProp) {
-            minder.off(handlerNameToEventName(handlerProp), props[handlerProp])
-          }
-        })
-      }
+      forEachHandler(minder, props, (eventName) => {
+        minder.off(eventName, handlers[eventName])
+      })
     }
   }, [minder, ...handlerPropKeys.map((handlerProp) => props[handlerProp])])
 }
