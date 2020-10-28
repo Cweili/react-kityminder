3110ba57f0489e4ddda77610e33b416cccbb6d00
Cweili <3weili@gmail.com>
2020-10-28 21:29:15 +0800

    fix: detached node getMinder() returns undefined

---
+++ src/extends.js
@@ -0,0 +1,19 @@
+const { kityminder } = window
+
+// fix: detached node getMinder() returns undefined
+kityminder.Minder.prototype.attachNode = function (node) {
+  const _this = this
+  const rc = _this.getRenderContainer()
+  node.traverse(function (current) {
+    current.attached = true
+    current.minder = _this
+    rc.addShape(current.getRenderContainer())
+  })
+  rc.addShape(node.getRenderContainer())
+  _this.fire('nodeattach', {
+    node
+  })
+}
+kityminder.Node.prototype.getMinder = function () {
+  return this.getRoot().minder || this.minder
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'kity'
 import 'kityminder-core'
 
 import Kityminder from './Kityminder'
+import './extends'
 
 export const kity = window.kity
 export const kityminder = window.kityminder
